743f4272a2c268ba2e2990982ea3eaab4b71ea34
Ericky2022 <erivaldo.pereira.santana2016@gmail.com>
2023-07-05 23:04:01 -0300

    Criação do método Criar Produto

--- frontend/src/app/components/product/product-create/product-create.component.ts
+++ frontend/src/app/components/product/product-create/product-create.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ProductService } from '../product.service';
 import { Router } from '@angular/router';
+import { Product } from '../product.model';
 
 @Component({
   selector: 'app-product-create',
@@ -9,14 +10,22 @@ import { Router } from '@angular/router';
 })
 export class ProductCreateComponent implements OnInit{
 
-    constructor(private productService: ProductService, private router: Router){}
-
-    ngOnInit(): void {
+    product: Product = {
+      name: '',
+      price: 0,
 
     }
 
+    constructor(private productService: ProductService, private router: Router){}
+
+    ngOnInit(): void {}
+
     createProduct(): void{
-      this.productService.showMessage('Operação executada com sucesso')
+      this.productService.create(this.product).subscribe(() =>{
+        this.productService.showMessage('Operação executada com sucesso')
+        this.router.navigate(['products'])
+      } )
+
     }
 
     cancel(): void{
--- frontend/src/app/components/product/product.service.ts
+++ frontend/src/app/components/product/product.service.ts
@@ -1,12 +1,16 @@
+import { HttpClient, HttpClientModule } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { MatSnackBar } from "@angular/material/snack-bar";
+import { Product } from './product.model';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProductService {
 
-  constructor(private snakbar: MatSnackBar) { }
+  baseUrl = "http://localhost:3001/products"
+  constructor(private snakbar: MatSnackBar, private http: HttpClient) { }
 
   showMessage(msg: string): void {
     this.snakbar.open(msg, 'X',
@@ -14,6 +18,10 @@ export class ProductService {
       horizontalPosition: "right",
       verticalPosition: "top"
     })
+  }
+
+  create(product: Product): Observable<Product>{
+      return this.http.post<Product>(this.baseUrl, product)
   }
 
 }
